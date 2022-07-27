c3a5cab8836763def63eb861bc2e0d6bf7a2b183
vmakar9 <zmerin212@gmail.com>
2022-07-27 14:06:44 +0300

    1

---
+++ hw2/nib.js
@@ -0,0 +1,77 @@
+let array=[10,20,30,"string",'javascript',"java","oktenschool",3.14,9.88,true];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
+console.log(array[9]);
+let books1 = {title:"Metro2033",pagecount:300,genre:"postapocalyptic"};
+let books2 = {title:"Metro2033",pagecount:300,genre:"postapocalyptic",authors:[
+        {name:"Dmytro",age:43}
+    ]};
+let users=[
+    {
+        name:"Gleb",
+        username:"Xleb",
+        password:"ngjmxzs1",
+    },
+    {
+        name:"Mykola",
+        username:"Derd",
+        password:"bgfh32",
+    },
+    {
+        name:"Boris",
+        username:"Johnson",
+        password:"GreatBritain",
+    },
+    {
+        name:"Ivan",
+        username:"Chort",
+        password:"Hell",
+    },
+    {
+        name:"Igor",
+        username:"nagibator228",
+        password:"qwerty",
+    },
+    {
+        name:"Vlad",
+        username:"What is Love",
+        password:"mbhugr23",
+    },
+    {
+        name:"Victor",
+        username:"Vitek",
+        password:"vmgore21",
+    },
+    {
+        name:"Alexander",
+        username:"Canya",
+        password:"Canyanagibator228",
+    },
+    {
+        name:"Jack",
+        username:"Jakson",
+        password:"bnjgds32",
+    },
+    {
+        name:"Sauron",
+        username:"Dark lord",
+        password:"Rings of Power",
+    },
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
